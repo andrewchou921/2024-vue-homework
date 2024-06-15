15a7b3870e1d07661c5dffa84bbef27d9ed45e78
wcc723 <agito723@gmail.com>
2024-06-15 11:32:40 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: process.env.NODE_ENV === 'production' ? '/2024-vue-homework/' : '/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
