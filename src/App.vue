704b6d8c1ef0f0e69ededb23477e668325ab3bf7
wcc723 <agito723@gmail.com>
2024-06-15 11:29:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/about">About</RouterLink> |
+          <RouterLink to="/week1">第一週作業</RouterLink> |
+          <RouterLink to="/week2">第二週作業</RouterLink> |
+          <RouterLink to="/week3">第三週作業</RouterLink> |
+        </nav>
+      </div>
+    </header>
+    
+    <RouterView />
+  </div>
+</template>
